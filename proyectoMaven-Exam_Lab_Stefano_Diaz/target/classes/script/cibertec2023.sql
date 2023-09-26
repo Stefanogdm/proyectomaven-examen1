89db191ab49ca2578df6229ba2ec31610e2191cd
Stefano Diaz <116049962+Stefanogdm@users.noreply.github.com>
2023-09-25 19:03:47 -0500

    cibertec2023.sql

--- proyectoMaven-Exam_Lab_Stefano_Diaz/target/classes/script/cibertec2023.sql
+++ proyectoMaven-Exam_Lab_Stefano_Diaz/target/classes/script/cibertec2023.sql
@@ -3,7 +3,6 @@
 -- Fecha : 29-Septiembre-2023
 -- Alumno : Stefano Gabriel Diaz Marcelo
 -- -----------------------------------------
-create database cibertec2023;
 
 use cibertec2023;
 
@@ -15,14 +14,15 @@ create table Empleados(
     Nombres VARCHAR(50) NOT NULL,
     Edad SMALLINT NOT NULL,
     Sexo CHAR(1) NOT NULL,
+    Salario INT NOT NULL,
     PRIMARY KEY(Id)
 );
 
-insert into Empleados(Apellidos,Nombres,Edad,Sexo) 
-values('RIOS FLORES','ANA MARIA',22,'F'),
-('BALLON URBANO','JOSE CARLOS',21,'M'),
-('ALVA BERRIOS','CLAUDIA DORIS',24,'F'),
-('PALACIOS MEZA','MATHIAS ALEJANDRO',23,'M'),
-('ZEGARRA LOPEZ','LORENA ESTEFANIA',21,'F');
+insert into Empleados(Apellidos,Nombres,Edad,Sexo,Salario) 
+values('LAGUNA OLIVAS','SAID CARLOS',22,'M',1500),
+('BARCO URBANO','ADRIAN CARLOS',21,'M',1600),
+('JALABA BROUS','JUANA DORIS',24,'F',2000),
+('MARTINEZ MEZA','KENIA MAJU',23,'F',2400),
+('SURRIGA LOPEZ','LORENA ESTEFANIA',21,'F',1700);
 
 select * from Empleados;
